6ffcc75c96c30ddc73f6cce3670d1d4b7a9a2dea
guille <guillermo.lopez@coderio.co>
2021-11-13 21:33:54 -0300

    add home screen. Home is showing the products

---
+++ lib/modules/home/ui/home_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tul_eco/api/repository/home_repository.dart';
+import 'package:tul_eco/modules/home/bloc/home_bloc.dart';
+import 'package:tul_eco/modules/home/widgets/home_products_list.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  static Route route() {
+    return MaterialPageRoute(
+      builder: (_) => const HomePage(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<HomeBloc>(
+      create: (_) => HomeBloc(repository: HomeRepository())
+        ..add(const HomeEventFetchProducts()),
+      child: const HomeView(),
+    );
+  }
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    context.read<HomeBloc>().add(const HomeEventFetchProducts());
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Productos'),
+      ),
+      body: BlocBuilder<HomeBloc, HomeState>(
+        builder: (context, state) {
+          if (state is HomeStateInProgress) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (state is HomeStateSuccess) {
+            return HomeProductsList(lista: state.products);
+          }
+          if (state is HomeStateFailure) {
+            return const Center(
+              child: Text('Error al obtener los datos'),
+            );
+          }
+          return const SizedBox();
+        },
+      ),
+    );
+  }
+}
