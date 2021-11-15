6e4b3b0513f3ec5a0ca13f95e3b1972c368302cc
guille <guillermo.lopez@coderio.co>
2021-11-14 23:40:58 -0300

    modifique la estructura de success como page y view, le agregue el willpop para limpiar el carrito si vuelven para atras y el pop y cleanCart al btn volver

--- lib/modules/success/ui/success_screen.dart
+++ lib/modules/success/ui/success_screen.dart
@@ -4,32 +4,49 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:tul_eco/modules/pedido/bloc/pedido_bloc.dart';
 import 'package:tul_eco/utils/routes/tul_eco_navigator.dart';
 
-class SuccessScreen extends StatelessWidget {
-  const SuccessScreen({Key? key}) : super(key: key);
+class SuccessPage extends StatelessWidget {
+  const SuccessPage({Key? key}) : super(key: key);
 
   static Route route() {
-    return MaterialPageRoute(builder: (_) => const SuccessScreen());
+    return MaterialPageRoute(builder: (_) => const SuccessPage());
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.amber[200],
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SvgPicture.asset('assets/img/done_icon.svg'),
-          const SizedBox(height: 20),
-          const Center(
-            child: Text('Datos enviados correctamente'),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
+    return const SuccessView();
+  }
+}
+
+class SuccessView extends StatelessWidget {
+  const SuccessView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        context.read<PedidoBloc>().add(const CleanCarrito());
+        return true;
+      },
+      child: Scaffold(
+        backgroundColor: Colors.amber[200],
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset('assets/img/done_icon.svg'),
+            const SizedBox(height: 20),
+            const Center(
+              child: Text('Datos enviados correctamente'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
               onPressed: () {
-                TulEcoNavigator.instance.goHome();
+                Navigator.pop(context);
+                context.read<PedidoBloc>().add(const CleanCarrito());
               },
-              child: const Text('VOLVER'))
-        ],
+              child: const Text('VOLVER'),
+            )
+          ],
+        ),
       ),
     );
   }
